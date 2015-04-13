52a3ee568fd72827a892cc2c9e77db726fb5bbcf
Gabriel Gonzalez <Gabriel439@gmail.com>
2015-04-12 21:16:43 -0700

    Added resugaring of context

--- src/Annah/Error.hs
+++ src/Annah/Error.hs
@@ -6,6 +6,7 @@ module Annah.Error (
     -- * Error
       TypeError(..)
     , TypeMessage(..)
+    , Context
     , resugarTypeError
     ) where
 
@@ -13,7 +14,7 @@ import Control.Exception (Exception)
 import Data.Monoid (mempty, (<>))
 import Data.Text.Lazy (Text, unpack)
 import qualified Data.Text.Lazy as Text
-import Data.Text.Lazy.Builder (toLazyText)
+import Data.Text.Lazy.Builder (fromLazyText, toLazyText)
 import Data.Typeable (Typeable)
 import qualified Morte.Core as M
 import Turtle (FilePath)
@@ -24,7 +25,7 @@ import Annah.Sugar (resugar)
 import Annah.Syntax (Expr)
 
 data TypeError m = TypeError
-    { context     :: M.Context
+    { context     :: Context m
     , current     :: Expr m
     , typeMessage :: TypeMessage m
     } deriving (Typeable)
@@ -42,12 +43,16 @@ data TypeMessage m
     | TypeMismatch (Expr m) (Expr m)
     | Untyped M.Const
 
+newtype Context m = Context { getContext :: [(Text, Expr m)] }
+
 instance Show (TypeMessage FilePath) where
     show = unpack . toLazyText . build
 
 resugarTypeError :: (M.Expr -> Maybe m) -> M.TypeError -> TypeError m
-resugarTypeError link (M.TypeError a b c) =
-    TypeError a (resugar link b) (resugarTypeMessage link c)
+resugarTypeError link (M.TypeError ctx curr msg) = TypeError
+    (resugarContext link ctx)
+    (resugar link curr)
+    (resugarTypeMessage link msg)
 
 resugarTypeMessage :: (M.Expr -> Maybe m) -> M.TypeMessage -> TypeMessage m
 resugarTypeMessage _     M.UnboundVariable      = UnboundVariable
@@ -60,6 +65,11 @@ resugarTypeMessage link (M.TypeMismatch e1 e2 ) =
     TypeMismatch (resugar link e1) (resugar link e2)
 resugarTypeMessage _    (M.Untyped c          ) = Untyped c
 
+resugarContext :: (M.Expr -> Maybe m) -> M.Context -> Context m
+resugarContext link ctx = Context (do
+    (txt, expr) <- ctx
+    return (txt, resugar link expr) )
+
 instance Builds TypeError where
     build (TypeError ctx curr msg)
         =   (   if Text.null (toLazyText ctx')
@@ -70,7 +80,7 @@ instance Builds TypeError where
         <>  "\n"
         <>  build msg
       where
-        ctx' = M.buildContext ctx
+        ctx' = build ctx
 
 instance Builds TypeMessage where
     build  UnboundVariable           =
@@ -92,6 +102,16 @@ instance Builds TypeMessage where
         <>  "Argument type: " <> build expr2 <> "\n"
     build (Untyped c)                =
             "Error: " <> M.buildConst c <> " has no type\n"
+
+instance Builds Context where
+    build
+        =   fromLazyText
+        .   Text.unlines
+        .   map (toLazyText . buildKV)
+        .   reverse
+        .   getContext
+      where
+        buildKV (key, val) = fromLazyText key <> " : " <> build val
 
 prettyTypeError :: TypeError FilePath -> Text
 prettyTypeError = toLazyText . build
