69b48c5c9ebf6a030e7e9a2b0fcc00899ca3b453
Gabriel Gonzalez <Gabriel439@gmail.com>
2016-04-09 20:46:23 -0700

    `annah` executable now takes an optional `Path`
    The rationale behind this is that `annah` should be able to desugar `*.annah`
    files in directories other than the current directory, but those field may
    have imports relative to their current location.  If you feed that file into
    `annah` via standard input that file's location information is lost and `annah`
    cannot correctly resolve the relative imports.  Supplying the file via a path
    ensures that `annah` can resolve imports relative to that path.

--- exec/Main.hs
+++ exec/Main.hs
@@ -2,36 +2,49 @@
 
 module Main where
 
-import qualified Annah.Core   as Annah
-import qualified Annah.Parser as Annah
 import Control.Applicative ((<|>))
 import Control.Exception (Exception, throwIO)
 import Data.Monoid (mempty)
 import Data.Text.Lazy (fromStrict)
-import qualified Data.Text.Lazy.IO as Text
-import qualified Morte.Core   as Morte
-import qualified Morte.Import as Morte
+import Morte.Core (Path(..))
 import Options.Applicative
 import System.IO (stderr)
+
+import qualified Annah.Core                as Annah
+import qualified Annah.Parser              as Annah
+import qualified Data.Text.Lazy.IO         as Text
+import qualified Filesystem.Path.CurrentOS as Filesystem
+import qualified Morte.Core                as Morte
+import qualified Morte.Import              as Morte
 
 throws :: Exception e => Either e a -> IO a
 throws (Left  e) = throwIO e
 throws (Right r) = return r
 
+parser :: Parser (Maybe String)
+parser = optional
+    (   strArgument
+        (   metavar "FILE"
+        <>  help "File to desugar"
+        )
+    )
+
 main :: IO ()
 main = do
-    execParser $ info (helper <*> pure ())
+    mFile <- execParser $ info (helper <*> parser)
         (   fullDesc
         <>  header "annah - A strongly typed, purely functional language"
         <>  progDesc "Annah is a medium-level language that is a superset of \
                      \Morte.  Use this compiler to desugar Annah code to Morte \
                      \code."
         )
-    txt <- Text.getContents
+    txt <- case mFile of
+        Nothing   -> Text.getContents
+        Just file -> Text.readFile file
     ae  <- throws (Annah.exprFromText txt)
     let me = Annah.desugar ae
     -- Only statically link the Morte expression for type-checking
-    me' <- Morte.load me
+    me' <- Morte.load (fmap (File . Filesystem.decodeString) mFile) me
     mt  <- throws (Morte.typeOf me')
     -- Return the dynamically linked Morte expression
     Text.putStrLn (Morte.pretty (Morte.normalize me))
